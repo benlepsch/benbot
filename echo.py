171a0de8e4a416dfc705bb2dc49db6af0e5899b4
benlepsch <blepsch@gmail.com>
2020-06-04 01:51:58 -0400

    can change pfp

--- echo.py
+++ echo.py
@@ -8,6 +8,11 @@ class Echo(discord.Client):
         print(self.user.id)
         print('-----------')
 
+        pfp = open('original_pfp.png','rb')
+        byte = pfp.read()
+        pfp.close()
+        await echo.user.edit(password=None,avatar=byte)
+
         game = discord.Game('VALORANT')
         await echo.change_presence(status=discord.Status.online, activity=game)
     
@@ -17,18 +22,22 @@ class Echo(discord.Client):
         
 
         # "echo" imitates a person on the server, copying their role, nickname, and status
+
+        # first unecho if it was already copying someone
         if message.content.startswith('echo'):
             for role in message.author.guild.me.roles:
                if role.name != 'not spam bot' and role.name != '@everyone':
                    await role.edit(name='BenBot', colour=discord.Colour(5).lighter_grey(), hoist=False)
                    await message.author.guild.me.edit(nick='BenBot')
 
+            # get the member it's copying from the message
             copying = message.content.split()[1]
-            copying = int(copying[3:len(copying)-1]) # id of person tagged in the command
+            copying = int(copying[3:len(copying)-1])
             guild = message.author.guild
             me = guild.me
-
             copying = guild.get_member(copying)
+            
+            # change nickname
             await me.edit(nick=copying.display_name)
             
             # get every role that the person i'm copying has that is visible in the sidebar and sort them to determine which to copy
@@ -44,12 +53,19 @@ class Echo(discord.Client):
                     top = role.position
                     tr = role
 
-            # edit the "benbot" role in the server to be the name/colour of the person im echoing
-            for role in me.roles:
-                if role.name != 'not spam bot' and role.name != '@everyone':
-                    await role.edit(name=tr.name, colour=tr.colour)
-                    await role.edit(hoist=True)
-                    break
+            if not tr == None:
+                # edit the "benbot" role in the server to be the name/colour of the person im echoing
+                for role in me.roles:
+                    if role.name != 'not spam bot' and role.name != '@everyone':
+                        await role.edit(name=tr.name, colour=tr.colour)
+                        await role.edit(hoist=True)
+                        break
+            
+            # next
+            # GET PROFILE PICTURE
+            # this is insane
+
+            await message.channel.send(copying.avatar_url)
 
         if message.content.startswith('unecho'):
            for role in message.author.guild.me.roles:
